3987ec006a1522f799b23da3b30beb0656e610c7
Durgesh Kumar <durgeshksm2000@gmail.com>
2023-07-24 23:05:55 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import React from 'react'
+import Hello from './Hello';
+
+const App = () => {
+  return (
+    <div>
+     <Hello/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Hello2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello2 = () => {
+  return (
+    <div>
+      Hello2
+    </div>
+  )
+}
+
+export default Hello2
